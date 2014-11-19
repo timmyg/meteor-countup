f0d91361a21a0ed496164f135913d199b25f666a
Tim Giblin <timmyg13@gmail.com>
2014-11-19 17:08:40 -0500

    client only

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'timmyg13:countup',
   summary: 'countUp.js v1.3.1 packaged for meteor',
-  version: '1.0.2',
+  version: '1.0.3',
   git: 'https://github.com/timmyg/meteor-countup'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('timmyg13:countup.js');
+  api.addFiles('timmyg13:countup.js', 'client');
 });
 
 Package.onTest(function(api) {
